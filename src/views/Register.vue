48a1938bb297a035644940d62e8a20f3d94ac1af
Junior <duartejusanca@gmail.com>
2022-04-11 21:04:50 +0200

    add facebook authentication

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -4,7 +4,11 @@
     <input type="text" v-model="email" /><br />
     <input type="password" v-model="password"/><br/><br>
     <button @click="Register">Connection</button><br>
-    <button @click="googleConnect"> google </button>
+    <button @click="googleConnect"> google </button><br>
+    <button @click="facebookConnect"> facebook </button><br>
+    <button @click="twitterConnect"> twitter </button>
+
+
     <p>You have an account ? You can connect <router-link to="/login">Login</router-link></p>
   </div>
 </template>
@@ -12,6 +16,7 @@
 <script>
 import firebase from 'firebase/compat/app'; 
 import 'firebase/compat/auth';
+
 
 export default {
   name: "RegisterView",
@@ -41,7 +46,22 @@ export default {
           }).catch((err) => {
             alert('Oops. ' + err.message)
           })
-        }
+        },
+          facebookConnect(){
+            const provider = new firebase.auth.FacebookAuthProvider();
+            firebase.auth().signInWithPopup(provider).then(() => {
+              this.$router.replace('home'); 
+              }).catch((err) => { 
+                alert('Oops. ' + err.message)
+          }) 
+        },
+        twitterConnect(){
+           const provider = new firebase.auth.TwitterAuthProvider();
+        firebase.auth().signInWithPopup(provider).then(() => {
+          this.$router.replace('home'); }).catch((err) => {
+             alert('Oops. ' + err.message)
+        }) },
+
       }
     }
 </script>
