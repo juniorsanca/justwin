42403de79849f5570ae33e975f1e196d65379d3f
Junior <duartejusanca@gmail.com>
2022-04-20 17:27:24 +0200

    put same size on all homepage butttons

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="home">
     
-    <div class="w-full h-screen bg-no-repeat bg-cover">
-
+    <div>
         <div class="px-1 py-2.5 text-center mr-0 mb-2">
         <div>
             <img alt="Vue logo" src="../public/assets/img/justwin.gif">
@@ -11,51 +10,56 @@
             <img alt="Vue logo" src="../public/assets/img/tv-nft.gif">
         </div><br>
 
-        <div class="grid grid-cols-2 flex space-x-4  justify-center ">
-          <button
-              type="button"
-              class="
-              font-sans text-center font-bold shadow-lg 
-              shadow-cyan-500/50 placeholder-white
-              bg-black bg-opacity-100 rounded-full 
-              border border-cyan-300 p-2 mb-6
-              text-white outline-none
-              "
-            >PLAY</button>
-          <button
-            type="button"
-            class="
-              font-sans text-center font-bold shadow-lg 
-              shadow-pink-500/50 placeholder-white
-              bg-black bg-opacity-100 rounded-full 
-              border border-pink-500 p-2 mb-6
-              text-white outline-none
-                "
-          >
-          <router-link to="/ranking" class="text-white">RANKINGS</router-link>
-          </button>
-
-          <button
-            type="button"
-            class="font-sans text-center font-bold shadow-lg 
-                    shadow-pink-500/50 placeholder-white
-                    bg-black bg-opacity-100 rounded-full border 
-                    border-pink-500 p-2 mb-6 text-white
-                    outline-none
+        <div class="grid grid-cols-2 flex space-x-2">
+         
+              <button
+                  type="button"
+                  class="
+                  font-sans text-center font-bold shadow-lg 
+                  shadow-cyan-500/50 placeholder-white
+                  bg-black bg-opacity-100 rounded-full 
+                  border border-cyan-300 p-2 mb-6
+                  text-white outline-none
+                  "
+                >PLAY</button>
+              
+              <button
+                type="button"
+                class="
+                  font-sans text-center font-bold shadow-lg 
+                  shadow-pink-500/50 placeholder-white
+                  bg-black bg-opacity-100 rounded-full 
+                  border border-pink-500 p-2 mb-6
+                  text-white outline-none
                     "
-          >HOW TO PLAY</button>
+              >
+              <router-link to="/ranking" class="text-white">RANKINGS</router-link>
+              </button>
+          </div>
 
-          <button
-            type="button"
-            @click="logout"
-            class="
-              font-sans text-center font-bold shadow-lg 
-              shadow-pink-500/50 placeholder-white
-              bg-black bg-opacity-100 rounded-full border 
-              border-pink-500 p-2 mb-6 text-white
-              outline-none
-              " 
-          >EXIT</button>          
+          <div class="grid grid-cols-2 flex space-x-2">
+              <button
+                type="button"
+                class="
+                        font-sans text-center font-bold shadow-lg 
+                        shadow-pink-500/50 placeholder-white
+                        bg-black bg-opacity-100 rounded-full 
+                        border border-pink-500 p-2 mb-6 
+                        text-white outline-none
+                        "
+              >HOW TO PLAY</button>
+
+              <button
+                type="button"
+                @click="logout"
+                class="
+                  font-sans text-center font-bold shadow-lg 
+                  shadow-pink-500/50 placeholder-white
+                  bg-black bg-opacity-100 rounded-full border 
+                  border-pink-500 p-2 mb-6 text-white
+                  outline-none
+                  " 
+              >EXIT</button>          
 
         </div> 
     </div>
@@ -78,7 +82,6 @@ export default {
         this.$router.replace('login')
       })
     },
-    
   },
 }
 </script>
